90dd8b0d7448f6ab94c8b43fbe6035b89ebc6cc7
Elias Gillgren <33657449+gilaseillgren@users.noreply.github.com>
2020-09-14 10:06:28 +0200

    removed files,code and added global style in conf

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,11 @@
 import React from "react"
-import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const index = () => {
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+    <div>
+      <h1>404</h1>
+    </div>
   )
 }
 
-export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default index
