677472e2aba2f16e5ed1713c43e48b925417174e
Elias Gillgren <33657449+gilaseillgren@users.noreply.github.com>
2020-09-14 10:46:26 +0200

    added a container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Container } from "../components"
 
 const index = () => {
   return (
-    <div>
-      <h1>hej</h1>
-    </div>
+    <Container>
+      <h1>HEllu World!</h1>
+    </Container>
   )
 }
 
